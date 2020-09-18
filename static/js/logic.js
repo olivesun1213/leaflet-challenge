13c4c15ee72a4eebdf97ebe9fb859dceae69953c
olivesun1213 <63970195+olivesun1213@users.noreply.github.com>
2020-09-18 11:13:05 -0400

    updates

--- static/js/logic.js
+++ static/js/logic.js
@@ -83,17 +83,20 @@ function createMap(earthquakes) {
         accessToken: API_KEY
     });
     var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 13,
-        id: 'mapbox.light',
-        accessToken: API_KEY
-    });
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 13,
+      id: "light-v10",
+      accessToken: API_KEY
+   });
 
-    var outdoors = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>',
-        maxZoom: 13,
-        id: 'mapbox.outdoors',
-        accessToken: API_KEY
+    
+
+   var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      maxZoom: 13,
+      id: "mapbox/streets-v11",
+      accessToken: API_KEY
+  
     });
    
       // Define a baseMaps object to hold our base layers
